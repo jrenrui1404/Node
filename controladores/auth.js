de12d128fdf67e572c0b2751f430dcf431e72e77
jrenrui1404 <jrenrui1404@g.educaand.es>
2023-02-20 09:50:03 +0100

    Validador

--- controladores/auth.js
+++ controladores/auth.js
@@ -1,14 +1,15 @@
 const {response} = require('express')
-
+const { validationResult } = require('express-validator')
 const registrar = (req, res = response) =>{
     const {nombre, email, password} = req.body 
-    if (password.length < 8) {
+    const errores = validationResult(req)
+    console.log(errores);
+    if (! errores.isEmpty()){
         return res.status(400).json({
-            ok: false,
-            mensaje: "la contraseña debe tener mínimo 8 caracteres"
+            ok : false,
+            mensaje : errores.mapped() 
         })
     }
-
     return res.json({
             ok : true,
             mensaje: "registro",
--- rutas/auth.js
+++ rutas/auth.js
@@ -1,5 +1,6 @@
-const {Router} = require('express')
+const {Router, request} = require('express')
 const { registrar, loguear} = require('../controladores/auth');
+const {check} = request('express-validator')
 const router = Router()
 
 //ruta raíz
@@ -7,7 +8,13 @@ router.get('/', (req, res) =>{
     res.send('Hello World MIL MARAVILLAS')
 })
 
-router.post("/registro", registrar)
+router.post("/registro", 
+    [
+        check('nombre', 'el nombre no puede estar vacío').notEmpty(),
+        check('email', 'email debe ser un email válido').isEmail(),
+        check('password', 'la contraseña debe tener al menos 8 carácteres').isLength({min : 8})
+    ],
+registrar)
 
 router.post("/login", loguear)
 
