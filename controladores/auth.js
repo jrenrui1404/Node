b66d11183e74e9530b5abdb053e20afd6fced17a
José María Rentero Ruiz <jrenrui1404@g.educaand.es>
2023-02-23 20:26:33 +0100

    Video 8 Principio

--- controladores/auth.js
+++ controladores/auth.js
@@ -1,15 +1,6 @@
 const {response} = require('express')
-const { validationResult } = require('express-validator')
 const registrar = (req, res = response) =>{
     const {nombre, email, password} = req.body 
-    const errores = validationResult(req)
-    console.log(errores);
-    if (! errores.isEmpty()){
-        return res.status(400).json({
-            ok : false,
-            mensaje : errores.mapped() 
-        })
-    }
     return res.json({
             ok : true,
             mensaje: "registro",
@@ -21,6 +12,14 @@ const registrar = (req, res = response) =>{
 
 const loguear = (req, res = response) =>{
     const {email, password} = req.body 
+    const errores = validationResult(req)
+    console.log(errores);
+    if (! errores.isEmpty()){
+        return res.status(400).json({
+            ok : false,
+            mensaje : errores.mapped()
+        }) 
+    }
     res.json({
         mensaje: "login",
         email,
--- rutas/auth.js
+++ rutas/auth.js
@@ -1,21 +1,28 @@
-const {Router, request} = require('express')
+const {Router} = require('express')
 const { registrar, loguear} = require('../controladores/auth');
-const { check } = require('express-validator')
-const router = Router()
+const { check } = require('express-validator');
+const { validarCampos } = require('../../middleware/validator');
+const router = Router();
 
 //ruta raíz
 router.get('/', (req, res) =>{
     res.send('Hello World MIL MARAVILLAS')
-})
+});
 
 router.post("/registro", 
     [
         check('nombre', 'el nombre no puede estar vacío').notEmpty(),
         check('email', 'email debe ser un email válido').isEmail(),
-        check('password', 'la contraseña debe tener al menos 8 carácteres').isLength({min : 8})
+        check('password', 'la contraseña debe tener al menos 8 carácteres').isLength({min:8}),
+        validarCampos        
     ],
-registrar)
+registrar);
 
-router.post("/login", loguear)
+router.post("/login",
+    [
+        check('email', 'el email debe ser un email válido').isEmail(),
+        check('password', 'la contraseña debe tener al menos 8 carácteres').isLength({min:8}),
+        validarCampos
+    ], loguear)
 
 module.exports = router
