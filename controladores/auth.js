f402b3913a8777c09d5df6cb8956af61b1842a9f
José María Rentero Ruiz <jrenrui1404@g.educaand.es>
2023-02-24 21:08:41 +0100

    Video 10 por el porsi

--- controladores/auth.js
+++ controladores/auth.js
@@ -3,14 +3,31 @@ const Usuario = require('../modelos/Usuario')
 
 const registrar = async (req, res = response) =>{
     //const {nombre, email, password} = req.body 
-    const usuario = new Usuario(req.body)
-    await usuario.save
-    return res.json({
-            ok : true,
-            mensaje: "registro",
-            nombre: usuario.nombre,
-            email: usuario.email
+    //const usuario = new Usuario(req.body)
+    const {email} = req.body
+    try{
+        let usuario = await Usuario.findOne({email})
+        //console.log(usuario)
+        if (usuario){
+            return res.status(400).json({
+                ok: false,
+                mensaje : 'usuario ya existe en el BD'
+            })
+        }
+        usuario = new Usuario(req.body)
+        await usuario.save
+        return res.status(201).json({
+                ok : true,
+                mensaje: "registro",
+                nombre: usuario.nombre,
+                email: usuario.email
     })    
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            mensaje: 'error en el servidor'
+        })
+    }    
 }
 
 const loguear = (req, res = response) =>{
